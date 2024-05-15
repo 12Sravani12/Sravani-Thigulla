2ad76030932673b5b1c5d1c8c49e5b3c50a6669d
12Sravani12 <141737977+12Sravani12@users.noreply.github.com>
2024-05-15 20:09:57 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main( String args[] ) {
+        String first = "Hello"; 
+        String second = "World";
+        String third = first + second;
+        System.out.println(third);
+        // yet another way to concatenate strings
+        first += second;
+        System.out.println(first);        
+    }
+}
