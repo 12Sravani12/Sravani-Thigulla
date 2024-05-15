4ca0385848ac87f99909317caa54063a344d63aa
Sravani Thigulla <141737977+12Sravani12@users.noreply.github.com>
2024-05-15 20:20:44 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,8 @@
+public class uppercase {
+  public static void main(String[] args) {
+       String text = "java";
+       String textUp =text.toUpperCase();
+       System.out.println("Before : "+text);
+        System.out.println("After : "+textUp);
+}
+}
