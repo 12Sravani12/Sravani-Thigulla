d7a105c7f716a95edaf6fb9acf6f85cd687c15b7
Sravani Thigulla <141737977+12Sravani12@users.noreply.github.com>
2024-05-15 20:19:25 +0530

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,8 @@
+class Replace{
+    public static void main( String args[] ) {
+         String str="Hello Everyone";
+         String newStr = str.replaceAll("e", "a"); 
+         System.out.println("Before :"+str);
+         System.out.println("After :"+newStr);
+ }
+}
