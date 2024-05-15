ebc0547328f79bd80d4320e98d093444fce431d9
Sravani Thigulla <141737977+12Sravani12@users.noreply.github.com>
2024-05-15 20:20:00 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,17 @@
+class Index {
+    public static void main( String args[] ) {
+    String item="banana";
+    int index = item.indexOf('a');
+    System.out.println("Index of first occurance of ('a') is:"+index);
+}
+}
+
+
+
+
+
+
+
+
+
+   
